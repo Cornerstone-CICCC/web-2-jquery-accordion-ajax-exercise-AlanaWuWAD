ef902a81c60d89268333d82e150b3c254dc7ce01
Alana <rcclewu@gmail.com>
2024-11-27 17:07:12 -0800

    done

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 $(function() {
-  // your code here
+  $('.accordion-header').on('click',function(){
+    $(this).siblings('div').slideUp()
+    if(!$(this).next().is(":visible")){
+      $(this).next().slideDown()
+    }
+  })
+
+  $('.todos button').on('click', function(){
+    $.ajax({
+      url:`https://dummyjson.com/todos`,
+      type:'GET',
+      contentType: 'application/json',
+
+      success:function(response){
+        $('ul').empty()
+        response.todos.forEach( todo => {
+          $('ul').append(`
+            <li> ${todo.todo}</li>
+            `)
+        })
+        
+        console.log(response.todos)
+      }
+    })
+  })
 })
+
